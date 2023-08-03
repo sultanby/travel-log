642bf0f7228920e8dd638c836df3afa8d582a112
sultanby <jemjasott@gmail.com>
2023-08-03 21:55:00 +0200

    error handler added

--- server/src/index.js
+++ server/src/index.js
@@ -3,12 +3,25 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const middlewares = require('./middlewares')
+
 const app = express();
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello world!'
+    });
+});
+
+//Not Found error and forwarding to error handler middleware
+app.use(middlewares.notFound);
+//Error handler
+app.use(middlewares.errorHandler);
 
 
 const port = process.env.PORT || 1337
