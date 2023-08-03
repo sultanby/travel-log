2d93ca5f499d83aefc27f33a99319c7180bc7479
sultanby <jemjasott@gmail.com>
2023-08-03 19:28:24 +0200

    server setup

---
+++ server/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const app = express();
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
+
+
+const port = process.env.PORT || 1337
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+});
